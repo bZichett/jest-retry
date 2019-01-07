7bb57e12decdfe7fa8adfdb052863d73b6d17100
Benjamin Zichettello <Benjamin.Zichettello@adp.com>
2019-01-07 13:48:02 -0500

    feature: add x passed out of y for minimal reporter

--- src/minimal-reporter.js
+++ src/minimal-reporter.js
@@ -1,28 +1,36 @@
 class MinimalReporter {
   constructor(globalConfig, options) {
     this.silent = globalConfig.silent
+    this.suiteProgressIndex = 0
     console.log("\n")
+  }
+
+  onRunStart(results, options) {
+    this.numTotalTestSuites = results.numTotalTestSuites
   }
 
   onTestResult(contexts, results) {
     const fail = !!results.numFailingTests
 
-    console.log(fail
-      ? `FAIL   `
-      : `PASS   `,
+    const progress = `${++this.suiteProgressIndex} / ${this.numTotalTestSuites}`
+    console.log((fail
+      ? `FAIL  `
+      : `PASS  `),
+      progress + ' ' +
       results.testResults[0].ancestorTitles[0]
     )
 
+    const spacer = `           `
     if (fail && !this.silent) {
       let failures = 0, successes = 0
-      results.testResults.forEach((result,) => {
+      results.testResults.forEach((result) => {
         if (result.status === 'failed') {
-          console.log("     ", ++failures, result.title)
+          console.log(spacer + ' ', ++failures, result.title)
         } else {
           successes++
         }
       })
-      console.log(`      ( ${successes} successful test${successes.length > 1 ? 's' : ''} )`)
+      console.log(spacer, ` ( ${successes} successful test${successes.length > 1 ? 's' : ''} )`)
     }
   }
 }
